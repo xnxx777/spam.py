bd96cab16183348f1e2cd2805caab8792598bbee
7xN7X <107777247+xnxx777@users.noreply.github.com>
2022-06-18 18:36:44 -0600

    Update spamwhatsapp.py

--- spamwhatsapp.py
+++ spamwhatsapp.py
@@ -1,7 +1,7 @@
 import pyautogui as pg
 import time
 import webbrowser
-webbrowser.open("https://web.whatsapp.com/send?phone=+503 6435 6467")
+webbrowser.open("https://web.whatsapp.com/send?phone= numero que pondras")
 time.sleep(5)
 
 for i in range(1000):
